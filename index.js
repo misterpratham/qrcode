508e6f7daa1c2726dc2d5c5be7bc5390c4e6f3fc
Pratham Basnet <prathambasnet963@gmail.com>
2024-12-26 17:26:38 +0545

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const qrcode = require("qrcode");
 http
 .createServer((req, res)=>{
     res.writeHead(200, {"Content-Type" : "text/html"});
-    qrcode.toDataURL("https://github.com/",(err,url)=>{
+    qrcode.toDataURL("https://github.com/misterpratham",(err,url)=>{
         if(err) console.log(err);
         res.end(`<img src="${url}"/>`);
     });
